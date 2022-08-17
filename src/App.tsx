656c897fe05676b1a1f54cd6aeb624a8a092a43b
pepev94 <60986337+pepev94@users.noreply.github.com>
2022-08-17 11:56:24 -0500

    fixed airtable missing keys

--- src/App.tsx
+++ src/App.tsx
@@ -4,31 +4,37 @@ import { useConnect, Config } from "react-pelm-connect";
 import { Button, Typography, Grid, Box, Paper, Stack } from "@mui/material";
 
 var Airtable = require("airtable");
-var base = new Airtable({ apiKey: "YOUR_API_KEY" }).base("appXNkwkvtb22NWZd");
+var base = new Airtable({ apiKey: "keyuxFeFBRShGCiWR" }).base(
+  "appXNkwkvtb22NWZd"
+);
+
+const sendToAirtable = (data: string) => {
+  base("Users").create(
+    [
+      {
+        fields: {
+          accessToken: data,
+        },
+      },
+    ],
+    function (err: any, records: any) {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      records.forEach(function (record: any) {
+        console.log(record.getId());
+      });
+    }
+  );
+};
 
 const config: Config = {
   connectToken:
     "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJhdXRoLXNlcnZlciIsImNyZWF0ZWRfYXQiOjE2NjAwNzIyNjcuNjEwNDg0MSwidXNlciI6IjAwMDAwMDEiLCJjbGllbnRfaWQiOiI3MzE4NzUxZi1jMzM3LTQ0N2QtYjdlYy00ODFjNjA2MDQzMWUifQ.yecrkuGlQ1AuZYo33sQhduKnlndLnl-GvLttBwml83PgZFUSblDTB9x65Y_8c20XSI7a1sxO1hCil8m2Sbg2kjZCY-Hbfa7sKjmcUbhNIBfgwxZOG70SOgXENJoDcyhxvgSe67opC-ylPLrkTWWbMSzT6c5WuVIz6GtMzX3PlroBS884M7QkTukme-b4qc3tuwiEAxF0HPnpdr1RMDRD35MfeKWLqU91HeNwVCc-lKaYmZk3uVs_bEsJibsuSg41LgVa1RiUjM4m8zM8CexpWD8cUJgAtdgoodZiQ6N0CW11Vbr6uvhqcLgaahjp8zEVq7lN5oXDk6xoyRHPyenoSA",
   onSuccess: (authorizationCode: string) => {
     console.log(authorizationCode);
-    base("Users").create(
-      [
-        {
-          fields: {
-            accessToken: authorizationCode,
-          },
-        },
-      ],
-      function (err: any, records: any) {
-        if (err) {
-          console.error(err);
-          return;
-        }
-        records.forEach(function (record: any) {
-          console.log(record.getId());
-        });
-      }
-    );
+    sendToAirtable(authorizationCode);
   },
   onExit: () => {},
 };
