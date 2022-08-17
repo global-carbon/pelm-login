9e8630a0626a09f34986c0442d46f9f6ea1f55eb
pepev94 <60986337+pepev94@users.noreply.github.com>
2022-08-17 11:48:19 -0500

    added sentry

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://a7dfe0b73ab6480fa36b8ee1d95f91a0@o1365095.ingest.sentry.io/6660480",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
